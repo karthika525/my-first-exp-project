04f7dd718ea38d10c4dd7e0f870cda8b1f00d62f
karthika525 <karthikaindrajith@gmail.com>
2025-09-25 20:30:19 +0530

    Getting data from Get Form

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-  var  students = ['Ajay', 'Vineeth','Sudheesh']
 
-  res.render("hello-world", {students: students});
+  res.render("hello-world");
+
 });
 
+//route for handling form submission
+
+router.get('/createUser', function(req, res) {
+  const email = req.query.email
+  res.render("form-data",{
+  email:email, 
+  allData:req.query
+ });
+
+});
 module.exports = router;
