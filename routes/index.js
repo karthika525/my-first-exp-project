20a6f9ef4e77f003b5e8101889e94f207ac54b6b
karthika525 <karthikaindrajith@gmail.com>
2025-09-25 20:05:24 +0530

    Conditionally displaying HTML

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 var express = require('express');
  var router = express.Router();
 
- router.get('/', function(req, res) {
-   res.render('hello-world');
- });
- module.exports = router;
+
+  router.get('/', function(req, res) {
+    var count = 25
+    res.render("hello-world", {count : count});
+  });
+
+  module.exports = router;
