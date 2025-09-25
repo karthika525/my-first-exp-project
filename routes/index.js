2cabf6c2547cd6e4a376b8b0963e4f0294310f60
karthika525 <karthikaindrajith@gmail.com>
2025-09-25 22:08:19 +0530

    Aggregation-count Documents

--- routes/index.js
+++ routes/index.js
@@ -13,8 +13,6 @@ router.get('/getUser', function (req,res) {
       res.status(500).send("Error fetching users");
     });
 });
-//Limit and skip
- User.find({},'email password -_id').skip(1).limit(2).then(data => {console.log(data);})
-//Sorting
- User.find({},'email password -_id').skip(1).limit(2).then(data => {console.log(data);})
+//aggregation
+ User.countDocuments().then(data => {console.log(data);})
 module.exports = router;
